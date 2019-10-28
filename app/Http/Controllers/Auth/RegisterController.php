c6fafbb1031abcb49c3b447ee3d391cc722ad5a2
Murat CAN <muratcan.php@gmail.com>
2019-10-28 17:04:51 +0300

    Assigning default role as editor while registering

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
-
+use Spatie\Permission\Models\Role;
 class RegisterController extends Controller
 {
     /*
@@ -63,10 +63,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+       $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+      return  $user->assignRole("Editor");
     }
 }
